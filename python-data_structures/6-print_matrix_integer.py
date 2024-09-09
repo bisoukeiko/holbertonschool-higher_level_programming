2ef9fdfd5611c79f1d899ae0cb29e23cb11c3236
bisoukeiko <87bisou@gmail.com>
2024-09-09 23:26:05 +0200

    fix code for espace

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,12 @@
 def print_matrix_integer(matrix=[[]]):
 
     for index1 in range(len(matrix)):
+
         for index2 in range(len(matrix[index1])):
-            print("{:d}".format(matrix[index1][index2]), end=" ")
+
+            print("{:d}".format(matrix[index1][index2]), end="")
+
+            if index2 < len(matrix[index1]) - 1:
+                print(" ", end="")
+
         print()
