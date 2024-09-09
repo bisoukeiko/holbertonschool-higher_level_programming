28838bb4980e03173eb4fc18271970769223d31b
bisoukeiko <87bisou@gmail.com>
2024-09-09 23:07:14 +0200

    remove increment index

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,4 @@ def print_matrix_integer(matrix=[[]]):
     for index1 in range(len(matrix)):
         for index2 in range(len(matrix[index1])):
             print("{:d}".format(matrix[index1][index2]), end=" ")
-            index2 += 1
-        index1 += 1
         print()
