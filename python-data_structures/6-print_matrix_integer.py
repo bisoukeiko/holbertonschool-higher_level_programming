a4153cd294073116b427d90bedd1c0177d8194fa
bisoukeiko <87bisou@gmail.com>
2024-09-09 23:02:15 +0200

    fix .format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
 
     for index1 in range(len(matrix)):
         for index2 in range(len(matrix[index1])):
-            print(matrix[index1][index2], end=" ")
+            print("{:d}".format(matrix[index1][index2]), end=" ")
             index2 += 1
         index1 += 1
         print()
