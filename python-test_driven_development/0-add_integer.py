a7fa034bd8e10c3d4cc722f5f95b8d4902b9b1b8
bisoukeiko <87bisou@gmail.com>
2024-09-12 13:07:40 +0200

    first commit

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This module provides functions to add integers.
+Functions:
+    add_integers(a, b): Returns the sum of two integers.
+"""
+
+def add_integer(a, b=98):
+
+    """
+    Returns the sum of two integers.
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+
+    elif type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
