ce0e6574056a537155da590a1ad183aa15a4aa18
bisoukeiko <87bisou@gmail.com>
2024-09-13 11:15:23 +0200

    first commit

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""  function that prints a square with the character # """
+
+
+def print_square(size):
+    """
+        Args:
+            size(integer): the size length of the square
+        Raises:
+            TypeError: If the size is not a integer.
+            ValueError: If the size is less than 0.
+    """
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for index in range(size):
+        print("#" * size)
