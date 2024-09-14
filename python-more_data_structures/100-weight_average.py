c4044796135550ac51e97d81bc3cafc33024abb7
bisoukeiko <87bisou@gmail.com>
2024-09-14 23:56:16 +0200

    first commit

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+
+    if not my_list:
+        return 0
+
+    total = 0
+    sum = 0
+
+    for index in range(len(my_list)):
+        total += my_list[index][0] * my_list[index][1]
+        sum += my_list[index][1]
+    
+    return total / sum
