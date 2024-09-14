d284fbf1699c2aeded64f8fd008285d0464e99d1
bisoukeiko <87bisou@gmail.com>
2024-09-14 23:03:19 +0200

    first commit

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+
+    def test_orderded_list(self):
+        test_list = [1, 2, 3, 4]
+        expected_value = 4
+        self.assertEqual(max_integer(test_list), expected_value)
+
+    def test_unorderded_list(self):
+        test_list = [2, 4, 1, 3]
+        expected_value = 4
+        self.assertEqual(max_integer(test_list), expected_value)
+
+    def test_one_element_list(self):
+        test_list = [5]
+        expected_value = 5
+        self.assertEqual(max_integer(test_list), expected_value)
+
+    def test_empty_list(self):
+        test_list = []
+        expected_value = None
+        self.assertEqual(max_integer(test_list), expected_value)
