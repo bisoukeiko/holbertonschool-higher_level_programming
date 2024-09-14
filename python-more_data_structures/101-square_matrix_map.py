34fdff3383e8ccb6e0a7cfa184e9ac8a12a65e48
bisoukeiko <87bisou@gmail.com>
2024-09-15 01:05:47 +0200

    fix code max 3 lines

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_map(matrix=[]):
-
-    def square(num):
-        return num ** 2
-
-    return list(map(lambda list_x: list(map(square, list_x)), matrix))
+    return list(map(lambda lx: list(map(lambda num: (num**2), lx)), matrix))
