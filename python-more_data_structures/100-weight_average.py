d24c7327162aee7203c996e4e0cb43e42b52dffa
bisoukeiko <87bisou@gmail.com>
2024-09-15 00:07:03 +0200

    fix pycodestyle

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -11,5 +11,5 @@ def weight_average(my_list=[]):
     for index in range(len(my_list)):
         total += my_list[index][0] * my_list[index][1]
         sum += my_list[index][1]
-    
+
     return total / sum
