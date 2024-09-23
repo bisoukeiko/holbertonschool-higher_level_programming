c52dd2a771413ba4ebb7ed20a27a8415db4e0152
bisoukeiko <87bisou@gmail.com>
2024-09-23 12:45:54 +0200

    first commit

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" check if the object is an instance of,
+    or if the object is an instance of a class
+"""
+
+
+def is_same_class(obj, a_class):
+    """ return True if the object is an instance of,
+        or if the object is an instance of a class that inherited from
+        the specified class ; otherwise False
+    """
+    return isinstance(obj, a_class)
