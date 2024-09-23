bb6b8e2b23ed0f6aaa544eae46b9f24f4658de0d
bisoukeiko <87bisou@gmail.com>
2024-09-23 22:46:09 +0200

    fix pycodestyle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,6 +2,7 @@
 """ a class Rectangle that inherits from BaseGeometry (7-base_geometry.py) """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """ a class Rectangle that inherits from BaseGeometry """
 
