48bea43cfbd79530afff06ff46a2a384e64e16d0
bisoukeiko <87bisou@gmail.com>
2024-09-23 12:27:42 +0200

    add doctest

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" check the object is exactly an instance of the specified class """
+
 
 def is_same_class(obj, a_class):
     """ return True if the object is exactly an instance of the specified class
