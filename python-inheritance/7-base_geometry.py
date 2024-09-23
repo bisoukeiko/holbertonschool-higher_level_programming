faf8ed5976d2bfb165d69dd6a032edfbfdcfaa85
bisoukeiko <87bisou@gmail.com>
2024-09-23 21:47:52 +0200

    fix code remove line

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,6 +13,5 @@ class BaseGeometry:
         """  validate value """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
