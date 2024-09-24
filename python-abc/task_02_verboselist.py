22cd645da408dc951f925812302a31f475829b8e
bisoukeiko <87bisou@gmail.com>
2024-09-24 16:05:41 +0200

    first commit

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" Define a class VerboseList that extends the Python list class """
+
+
+from typing import Iterable, SupportsIndex
+
+
+class VerboseList(list):
+    """ Define a class VerboseList that inherits from
+        the built-in list class
+    """
+
+    def append(self, item):
+        """ adding the item to the list """
+        super().append(item)
+        print("Added [{}] to the list.".format(item))
+
+    def extend(self, iterable):
+        """ extending the list """
+        super().extend(iterable)
+        print("Extended the list with [{}] items.".format(len(iterable)))
+
+    def remove(self, value):
+        """ removing the item from the list """
+        if value in self:
+            print("Removed [{}] from the list".format(value))
+            super().remove(value)
+
+    def pop(self, index=-1):
+        """ popping the item from the list """
+        if index >= -1 and index < len(self):
+            print("Popped [{}] from the list.".format(self[index]))
+            super().pop(index)
