4cdc40e3913b019ab03ee8389d9e54bc7275991a
bisoukeiko <87bisou@gmail.com>
2024-09-24 12:18:40 +0200

    first commit

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Define an abstract class and two subclasses of Animal """
+from abc import ABCMeta, abstractmethod
+
+
+class Animal(metaclass=ABCMeta):
+    """ Define the Animal class """
+
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    """ a subclass Dog that inherits from the Animal class """
+
+    def sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+    """ a subclass Cat that inherits from the Animal class """
+
+    def sound(self):
+        return "Meow"
