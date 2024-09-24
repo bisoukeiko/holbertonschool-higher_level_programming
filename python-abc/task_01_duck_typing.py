af084f27729f7adcea647c38f1e3050aa08109a6
bisoukeiko <87bisou@gmail.com>
2024-09-24 14:54:52 +0200

    first commit

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+""" Define an abstract class and two subclasses of Shape """
+from abc import ABCMeta, abstractclassmethod
+import math
+
+
+class Shape(metaclass=ABCMeta):
+    """ Define the Shape class """
+
+    @abstractclassmethod
+    def area(self):
+        pass
+
+    @abstractclassmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    """ Define the Circle class """
+
+    def __init__(self, radius):
+        """ Instantiation radius """
+        self.radius = radius
+
+    def area(self):
+        """ Return the area of the circle """
+        return self.radius ** 2 * math.pi
+
+    def perimeter(self):
+        """ Return the perimeter of the circle """
+        return self.radius * 2 * math.pi
+
+
+class Rectangle(Shape):
+    """ Define the Rectangle class """
+
+    def __init__(self, width, height):
+        """ Instantiation width and height """
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """ Return the area of the rectangle """
+        return self.width * self.height
+
+    def perimeter(self):
+        """ Return the perimeter of the rectangle """
+        return (self.width * 2) + (self.height * 2)
+
+
+def shape_info(obj):
+    """ call area and perimeter methods of the passed object """
+    print("Area: {}".format(obj.area()))
+    print("Perimeter: {}".format(obj.perimeter()))
