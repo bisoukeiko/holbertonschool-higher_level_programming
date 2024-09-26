1f09ad5379c910acd12bd650433f2d88f184d1e7
bisoukeiko <87bisou@gmail.com>
2024-09-26 12:00:44 +0200

    fix string for print

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -10,17 +10,17 @@ class VerboseList(list):
     def append(self, item):
         """ adding the item to the list """
         super().append(item)
-        print("Added [{}] to the list".format(item))
+        print("Added [{}] to the list.".format(item))
 
     def extend(self, iterable):
         """ extending the list """
         super().extend(iterable)
-        print("Extended the list with [{}] items".format(len(iterable)))
+        print("Extended the list with [{}] items.".format(len(iterable)))
 
     def remove(self, value):
         """ removing the item from the list """
         if value in self:
-            print("Removed [{}] from the list".format(value))
+            print("Removed [{}] from the list.".format(value))
             super().remove(value)
         else:
             raise ValueError("[{}] not in list".format(value))
@@ -31,6 +31,6 @@ class VerboseList(list):
             raise IndexError("list is empty.")
         elif index >= -1 and index < len(self):
             print("Popped [{}] from the list".format(self[index]))
-            super().pop(index)
+            return super().pop(index)
         else:
             raise IndexError("list index[{}] out of range".format(index))
