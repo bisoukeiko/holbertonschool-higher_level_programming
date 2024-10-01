fea09b0d328e53efa19fd942004f8c750e0c101d
bisoukeiko <87bisou@gmail.com>
2024-10-01 17:24:39 +0200

    fix code add "" for the end of print

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" Define the function that reads a text file and prints it """
+
 
 def read_file(filename=""):
+    """ reads a text file (UTF8) and prints it to stdout """
     with open(filename, encoding="utf-8") as file:
         read_data = file.read()
-        print(read_data)
+        print(read_data, end="")
