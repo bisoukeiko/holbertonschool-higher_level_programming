f04d69384e8edfdcdbf22454061f9f006ca4ba60
bisoukeiko <87bisou@gmail.com>
2024-10-02 22:07:35 +0200

    first commit

---
+++ python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Define a class Student that defines a student """
+
+
+class Student():
+    """ a class Student that defines a student """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initialize a new student """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieve a dictionary representation of a Student instance """
+        dict = self.__dict__
+        new_dict = {}
+
+        if type(attrs) is list:
+            for key in attrs:
+                if key in dict:
+                    new_dict[key] = dict[key]
+
+            return new_dict
+        else:
+            return dict
+
+    def reload_from_json(self, json):
+        """ Replace all attributes of the Student instance """
+        dict = self.__dict__
+
+        for key in json:
+            if key in dict:
+                dict[key] = json[key]
