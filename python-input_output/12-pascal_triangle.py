a83c64f9c0963ef9b5cf61fa052f9b3b0e04d85b
bisoukeiko <87bisou@gmail.com>
2024-10-03 10:51:39 +0200

    first commit

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    task12 
+"""
+
+
+def pascal_triangle(n):
+    """ Define a function that returns a list of lists of integers
+        representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1 for col in range(row)] for row in range(1, n + 1)]
+
+    for row in range(2, n):
+        for col in range(1, row):
+            triangle[row][col] = triangle[row-1][col] + triangle[row-1][col-1]
+
+    return triangle
