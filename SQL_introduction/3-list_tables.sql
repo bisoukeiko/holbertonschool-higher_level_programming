51a75141d66564c8deeb8fbec7c42627b9781f22
bisoukeiko <87bisou@gmail.com>
2024-10-28 14:56:36 +0100

    write one line

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,4 +1,2 @@
 --  list all the tables of a database
-SELECT table_name
-FROM information_schema.tables
-WHERE table_type='BASE TABLE';
+SELECT table_name FROM information_schema.tables WHERE table_type='BASE TABLE';
