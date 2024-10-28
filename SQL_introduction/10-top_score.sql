1eba764e9ca0dcbfc267e96de342fc6aa393de33
bisoukeiko <87bisou@gmail.com>
2024-10-28 16:20:01 +0100

    first commit

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+--  list all records of the table second_table
+-- display both the score and the name (in this order)
+-- ordered by score (top first) 
+SELECT score, name 
+  FROM second_table
+ORDER BY score DESC;
