a1324dd27b2e26ac0ba2f17523ba21acf54aa9c4
bisoukeiko <87bisou@gmail.com>
2024-10-29 10:21:49 +0100

    first commit

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- list all the cities of California that can be found in the database hbtn_0d_usa
+SELECT cities.id, cities.name
+  FROM cities,
+       states
+ WHERE states.name = "California"
+   AND cities.state_id = states.id
+ORDER BY cities.id ASC;
