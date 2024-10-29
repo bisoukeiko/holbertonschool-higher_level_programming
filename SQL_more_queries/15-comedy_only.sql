f8a34adee12866165399c9230f9651986a5b8161
bisoukeiko <87bisou@gmail.com>
2024-10-29 12:18:42 +0100

    first commit

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+--  lists all Comedy shows in the database hbtn_0d_tvshows
+SELECT tv_shows.title
+  FROM tv_genres,
+       tv_show_genres,
+       tv_shows
+ WHERE tv_genres.id = tv_show_genres.genre_id
+   AND tv_show_genres.show_id = tv_shows.id
+   AND tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
