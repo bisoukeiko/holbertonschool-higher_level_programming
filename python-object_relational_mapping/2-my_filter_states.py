59863215e7d91016c131e3bbfa801bf0b2da0f57
bisoukeiko <87bisou@gmail.com>
2024-10-30 09:45:20 +0100

    add docstring

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" Python - Object-relational mapping
-    task02:
+""" task02:
+    takes in an argument and displays all values in the states table
+    of hbtn_0e_0_usa where name matches the argument
 """
 import MySQLdb
 import sys
@@ -40,8 +41,4 @@ def main():
 
 
 if __name__ == "__main__":
-    """
-    This conditional block checks if the current script is
-    being run as the main program.
-    """
     main()
