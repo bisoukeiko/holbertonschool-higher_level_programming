d0374a51574e63fa715660563b0e354882a7b3f4
bisoukeiko <87bisou@gmail.com>
2024-10-30 19:26:45 +0100

    first commit

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Python - Object-relational mapping
+    task12
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    """
+    Change the name of a State object where id = 2 to New Mexico
+    from the database hbtn_0e_6_usa.
+
+    Args:
+        sys.argv[1] (str): Username for MySQL authentication.
+        sys.argv[2] (str): Password for MySQL authentication.
+        sys.argv[3] (str): Database name to connect to.
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    SessionClass = sessionmaker(bind=engine)
+    session = SessionClass()
+
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = 'New Mexico'
+    session.commit()
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
