844be6b29ce6c87c30b6d68fd53c7373adc6571e
bisoukeiko <87bisou@gmail.com>
2024-10-30 19:33:53 +0100

    first commit

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Python - Object-relational mapping
+    task13
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    """
+    Delete all State objects with a name containing the letter 'a'
+    from the database hbtn_0e_6_usa.
+
+    Args:
+        sys.argv[1] (str): Username for MySQL authentication.
+        sys.argv[2] (str): Password for MySQL authentication.
+        sys.argv[3] (str): Database name to connect to.
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    SessionClass = sessionmaker(bind=engine)
+    session = SessionClass()
+
+    session.query(State).filter(State.name.like('%a%')).delete()
+    # state = session.query(State).filter(State.name.like('%a%')).delete()
+    # state.name = 'New Mexico'
+    session.commit()
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
