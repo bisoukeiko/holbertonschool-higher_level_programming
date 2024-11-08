78120d29c3ffc14fb65ee6331b9d0627c0ec67bd
bisoukeiko <87bisou@gmail.com>
2024-11-08 12:29:03 +0100

    fix pycodestyle

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -17,7 +17,7 @@ def generate_invitations(template, attendees):
     if type(attendees) is not list:
         print('Error: attendees must be a list of dictionaries')
         return
-    
+
     index = 1
     for dict_attendee in attendees:
         if type(dict_attendee) is not dict:
@@ -27,13 +27,17 @@ def generate_invitations(template, attendees):
         name = dict_attendee.get('name')
         event_title = dict_attendee.get('event_title')
         event_date = dict_attendee.get('event_date')
-        event_location= dict_attendee.get('event_location')
+        event_location = dict_attendee.get('event_location')
 
         invitation = template
         invitation = invitation.replace('{name}', name if name else "N/A")
-        invitation = invitation.replace('{event_title}', event_title if event_title else "N/A")
-        invitation = invitation.replace('{event_date}', event_date if event_date else "N/A")
-        invitation = invitation.replace('{event_location}', event_location if event_location else "N/A")
+        invitation = invitation.replace('{event_title}',
+                                        event_title if event_title else "N/A")
+        invitation = invitation.replace('{event_date}',
+                                        event_date if event_date else "N/A")
+        invitation = invitation.replace('{event_location}',
+                                        event_location
+                                        if event_location else "N/A")
 
         filename = 'output_' + str(index) + '.txt'
         try:
